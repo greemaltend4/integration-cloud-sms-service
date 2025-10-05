de5195a107e4f231dfbccd89862e0550ddbfddff
greemaltend4 <dengestabiy@hotmail.com>
2025-10-05 17:38:01 +0000

    feat: initial commit for config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    smsProviders: [
+        { name: 'Provider1', apiKey: process.env.PROVIDER1_API_KEY },
+        { name: 'Provider2', apiKey: process.env.PROVIDER2_API_KEY }
+    ],
+};
